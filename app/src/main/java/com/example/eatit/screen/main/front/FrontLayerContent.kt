e7f0b090e2f2e336f460e2396fbd7d87875e4310
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2021-01-21 02:11:37 +0700

    Provoide subtitle

--- app/src/main/java/com/example/eatit/screen/main/front/FrontLayerContent.kt
+++ app/src/main/java/com/example/eatit/screen/main/front/FrontLayerContent.kt
@@ -1,7 +1,17 @@
 package com.example.eatit.screen.main.front
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.eatit.screen.main.front.screen.FrontCategoriesScreen
 import com.example.eatit.screen.main.front.screen.FrontFavoriteScreen
 import com.example.eatit.screen.main.front.screen.FrontMainScreen
@@ -10,11 +20,25 @@ import com.example.eatit.screen.main.front.screen.FrontProfileScreen
 @Composable
 fun FrontLayerContent(frontScreen: FrontScreen) {
 	Crossfade(current = frontScreen) {
-		when (frontScreen) {
-			FrontScreen.MAIN -> FrontMainScreen()
-			FrontScreen.CATEGORIES -> FrontCategoriesScreen()
-			FrontScreen.FAVORITE -> FrontFavoriteScreen()
-			FrontScreen.PROFILE -> FrontProfileScreen()
+		Column {
+			Box(
+				modifier = Modifier
+					.heightIn(32.dp)
+					.padding(start = 16.dp, top = 12.dp, bottom = 12.dp),
+				contentAlignment = Alignment.Center
+			) {
+				Text(
+					text = frontScreen.title,
+					style = MaterialTheme.typography.h6,
+				)
+			}
+			Divider()
+			when (frontScreen) {
+				FrontScreen.MAIN -> FrontMainScreen()
+				FrontScreen.CATEGORIES -> FrontCategoriesScreen()
+				FrontScreen.FAVORITE -> FrontFavoriteScreen()
+				FrontScreen.PROFILE -> FrontProfileScreen()
+			}
 		}
 	}
 }
