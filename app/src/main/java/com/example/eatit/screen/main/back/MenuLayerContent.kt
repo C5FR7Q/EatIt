d80285575161a34503b579accbc2f66434395034
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2021-01-21 01:59:52 +0700

    Rework menu items.

--- app/src/main/java/com/example/eatit/screen/main/back/MenuLayerContent.kt
+++ app/src/main/java/com/example/eatit/screen/main/back/MenuLayerContent.kt
@@ -1,16 +1,20 @@
 package com.example.eatit.screen.main.back
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProvideTextStyle
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.example.eatit.screen.main.front.FrontScreen
 import com.example.eatit.screen.main.front.title
@@ -22,35 +26,53 @@ fun MenuBackLayer(
 ) {
 	Column(
 		modifier = Modifier
-			.padding(bottom = 24.dp, start = 57.dp, end = 57.dp)
+			.padding(bottom = 16.dp)
 			.fillMaxWidth(),
 		horizontalAlignment = Alignment.CenterHorizontally,
 	) {
-		ProvideTextStyle(value = MaterialTheme.typography.h5) {
-			FrontScreen.values().forEach { frontScreen ->
-				val name = frontScreen.title
-				SelectableText(
-					modifier = Modifier.padding(15.dp),
-					text = name,
-					isSelected = frontScreen == selectedFrontScreen,
-					onSelect = { onSelectFrontScreen(frontScreen) }
-				)
-			}
+		FrontScreen.values().forEach { frontScreen ->
+			val name = frontScreen.title
+			MenuItem(
+				modifier = Modifier.padding(bottom = 8.dp),
+				text = name,
+				isSelected = frontScreen == selectedFrontScreen,
+				onSelect = { onSelectFrontScreen(frontScreen) }
+			)
 		}
 	}
 }
 
-// TODO: 07.01.2021 Move to common; can be reused
 @Composable
-private fun SelectableText(
-	modifier: Modifier = Modifier,
+private fun MenuItem(
+	modifier: Modifier,
 	text: String,
+	icon: ImageVector = Icons.Default.Favorite,
 	isSelected: Boolean,
-	onSelect: (String) -> Unit
+	onSelect: () -> Unit
 ) {
-	Text(
-		modifier = Modifier.clickable(onClick = { onSelect(text) }).then(modifier),
-		text = text,
-		textDecoration = if (isSelected) TextDecoration.Underline else null
-	)
+	Surface(
+		modifier = Modifier
+			.preferredHeight(48.dp)
+			.padding(horizontal = 8.dp)
+			.fillMaxWidth()
+			.then(modifier),
+		shape = MaterialTheme.shapes.small,
+		color = if (isSelected) MaterialTheme.colors.secondary.copy(alpha = 0.3f) else Color.Transparent
+	) {
+		Row(
+			modifier = Modifier
+				.clickable(onClick = onSelect)
+				.alpha(if (isSelected) 1f else 0.3f),
+			verticalAlignment = Alignment.CenterVertically
+		) {
+			Icon(
+				imageVector = icon,
+				modifier = Modifier.padding(start = 8.dp, end = 24.dp)
+			)
+			Text(
+				text = text,
+				style = MaterialTheme.typography.subtitle1
+			)
+		}
+	}
 }
