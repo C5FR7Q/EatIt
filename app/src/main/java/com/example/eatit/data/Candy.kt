461f897a271b29decb951ad7c367a90e1c33fd04
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2021-01-31 18:28:02 +0700

    Fulfill Candy

--- app/src/main/java/com/example/eatit/data/Candy.kt
+++ app/src/main/java/com/example/eatit/data/Candy.kt
@@ -3,11 +3,22 @@ package com.example.eatit.data
 data class Candy(
 	val name: String,
 	val price: Float,
+	val calorie: Int,
+	val sweetness: Sweetness,
 	val imageUrl: String
 )
 
-internal fun simpleCandy(name: String, price: Float) = Candy(
+internal fun simpleCandy(name: String, price: Float, calorie: Int, sweetness: Sweetness) = Candy(
 	name,
 	price,
+	calorie,
+	sweetness,
 	"https://images.unsplash.com/photo-1586985289688-ca3cf47d3e6e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
 )
+
+enum class Sweetness {
+	NOT_SWEET,
+	SLIGHTLY_SWEET,
+	SWEET,
+	VERY_SWEET
+}
--- app/src/main/java/com/example/eatit/data/CandyCategory.kt
+++ app/src/main/java/com/example/eatit/data/CandyCategory.kt
@@ -15,54 +15,54 @@ object CandyCategories {
 	val CAKE = CandyCategory(
 		"Cake",
 		listOf(
-			simpleCandy("Fruity cake", 12.99f),
-			simpleCandy("Antonof's cake", 17.49f),
-			simpleCandy("Napoleon", 17.19f),
+			simpleCandy("Fruity cake", 12.99f, 350, Sweetness.VERY_SWEET),
+			simpleCandy("Antonof's cake", 17.49f, 322, Sweetness.NOT_SWEET),
+			simpleCandy("Napoleon", 17.19f, 588, Sweetness.SWEET),
 		)
 	)
 
 	val CHEESECAKE = CandyCategory(
 		"Cheesecake",
 		listOf(
-			simpleCandy("Classical cheesecake", 20.89f),
-			simpleCandy("Snickers cheesecake", 23.59f),
+			simpleCandy("Classical cheesecake", 20.89f, 712, Sweetness.NOT_SWEET),
+			simpleCandy("Snickers cheesecake", 23.59f, 951, Sweetness.SLIGHTLY_SWEET),
 		)
 	)
 
 	val COOKIE = CandyCategory(
 		"Cookie",
 		listOf(
-			simpleCandy("Chocolate cookie", 2.14f),
-			simpleCandy("Almond cookie", 3.24f),
-			simpleCandy("Prince of Persia", 3.14f),
-			simpleCandy("Godzilla", 5.42f),
+			simpleCandy("Chocolate cookie", 2.14f, 68, Sweetness.SWEET),
+			simpleCandy("Almond cookie", 3.24f, 72, Sweetness.SLIGHTLY_SWEET),
+			simpleCandy("Prince of Persia", 3.14f, 108, Sweetness.SLIGHTLY_SWEET),
+			simpleCandy("Godzilla", 5.42f, 121, Sweetness.NOT_SWEET),
 		)
 	)
 
 	val CUPCAKE = CandyCategory(
 		"Cupcake",
 		listOf(
-			simpleCandy("Classical cupcake", 8.99f),
+			simpleCandy("Classical cupcake", 8.99f, 389, Sweetness.SWEET),
 		)
 	)
 
 	val ECLAIR = CandyCategory(
 		"Eclair",
 		listOf(
-			simpleCandy("Banana eclair", 5.74f),
-			simpleCandy("Strawberry eclair", 5.74f),
-			simpleCandy("Blueberry eclair", 5.74f),
-			simpleCandy("Vanilla eclair", 5.74f),
-			simpleCandy("Chocolate eclair", 5.74f),
+			simpleCandy("Banana eclair", 5.74f, 451, Sweetness.VERY_SWEET),
+			simpleCandy("Strawberry eclair", 5.74f, 469, Sweetness.SWEET),
+			simpleCandy("Blueberry eclair", 5.74f, 463, Sweetness.SWEET),
+			simpleCandy("Vanilla eclair", 5.74f, 431, Sweetness.SLIGHTLY_SWEET),
+			simpleCandy("Chocolate eclair", 5.74f, 492, Sweetness.SWEET),
 		)
 	)
 
 	val PIE = CandyCategory(
 		"Pie",
 		listOf(
-			simpleCandy("Salmon pie", 30.22f),
-			simpleCandy("Pie with potato and egg", 22.11f),
-			simpleCandy("Khachapuri", 27.13f),
+			simpleCandy("Salmon pie", 22.22f, 492, Sweetness.NOT_SWEET),
+			simpleCandy("Pie with potato and egg", 18.11f, 967, Sweetness.NOT_SWEET),
+			simpleCandy("Khachapuri", 20.13f, 1214, Sweetness.NOT_SWEET),
 		)
 	)
 
